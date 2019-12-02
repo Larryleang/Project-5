debdbf726d0649d99209b2d754e5277897d402ee
Larryleang <larryleang@yahoo.com>
2019-12-02 14:37:47 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -40,6 +40,14 @@ const model = createModel();
 tfvis.show.modelSummary({name: 'Model Summary'}, model);
 }
 
+// Convert the data to a form we can use for training.
+const tensorData = convertToTensor(data);
+const {inputs, labels} = tensorData;
+    
+// Train the model  
+await trainModel(model, inputs, labels);
+console.log('Done Training');
+
 function createModel() {
   // Create a sequential model
   const model = tf.sequential(); 
